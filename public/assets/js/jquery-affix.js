c884caa0d86fa1815e463b9e1b1228e4ec3914d0
verhulstm <michael@terminallabs.com>
2021-02-12 10:09:21 -0600

    moved dirs

--- public/assets/js/jquery-affix.js
+++
@@ -1,6 +0,0 @@
-$(window).on('scroll', function(event) {
-  var scrollValue = $(window).scrollTop();
-  if (scrollValue == settings.scrollTopPx || scrollValue > 70) {
-       $('.bs-docs-sidebar').addClass('fixed-top');
-  } 
-});
